9fa629b282ba5bb969acc5df52fe15c777367227
Rishu Kumar <rishu.kumar@maersk.com>
2023-08-04 15:11:06 +0530

    added database connection with h2 and MySQL

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10001,'imrishuroy', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10002,'imrishuroy', 'Get Azure Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10003,'imrishuroy', 'Get GCP Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, TARGET_DATE, DONE)
+values(10004,'imrishuroy', 'Learn DevOps', CURRENT_DATE(), false);
